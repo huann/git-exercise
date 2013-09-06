4b43aff71b940c5d16636b1d9c3fd2da621b3504
huann <huann@grinnell.edu>
2013-09-05 19:41:14 -0500

    new print statement

--- src/edu/grinnell/csc207/huann/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/huann/hello/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
         System.out.println("Ann was here");
+        System.out.println("If you're reading this...");
     } // main(String[])
 }
